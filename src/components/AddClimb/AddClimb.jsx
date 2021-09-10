4ad66a467749803162c8286cfb758d714c4d208b
Curtis Kettler <curtiskettler@gmail.com>
2021-09-10 09:05:12 -0700

    toggle buttons added to form

--- src/components/AddClimb/AddClimb.jsx
+++ src/components/AddClimb/AddClimb.jsx
@@ -4,6 +4,8 @@ import { useSelector } from 'react-redux';
 import { CircularInput, CircularTrack, CircularProgress, CircularThumb, } from 'react-circular-input'
 import Checkbox from '@material-ui/core/Checkbox';
 import Button from '@material-ui/core/Button';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 
 
 function AddClimb() {
@@ -15,25 +17,10 @@ function AddClimb() {
     const [value, setValue] = useState(0.1);
     const [grade, setGrade] = useState('5.7');
     const [isFlash, setIsFlash] = useState(false);
-    const [flashChecked, setFlashChecked] = useState(false);
     const [isOnSight, setIsOnSight] = useState(false);
-    const [onSightChecked, setOnSightChecked] = useState(false);
     const [isBonus, setIsBonus] = useState(false);
-    const [bonusChecked, setBonusChecked] = useState(false);
+    const [bonusPoints, setBonusPoints] = useState(0);
     const [score, setScore] = useState(10);
-
-    //parameters for choose climb dropdown
-    const chooseClimbType = (event) => {
-        setClimbType(event.target.value);
-        if (climbType === 'Auto Belay') {
-            return score;
-        } else if (climbType === 'Top Rope') {
-            setScore(score + 1);
-        } else if (climbType === 'Lead'){
-            setScore(score + 2);
-        }
-        return score;
-    }
 
     const compareGrade = () => {
         if((value * 10) > user.gradeLevel){
@@ -53,86 +40,69 @@ function AddClimb() {
         setValue(stepValue(v));
         switch(value) {
             case 0.1:
-                compareGrade();
                 setGrade('5.7')
                 break;
             case 0.2:
-                compareGrade();
                 setGrade('5.8')
                 break;
             case 0.3:
-                compareGrade();
                 setGrade('5.9')
                 break;
             case 0.4:
-                compareGrade();
                 setGrade('5.10')
                 break;
             case 0.5:
-                compareGrade();
                 setGrade('5.11-')
                 break;
             case 0.6:
-                compareGrade();
                 setGrade('5.11+')
                 break;
             case 0.7:
-                compareGrade();
                 setGrade('5.12-')
                 break;
             case 0.8:
-                compareGrade();
                 setGrade('5.12+')
                 break;
             case 0.9:
-                compareGrade();
                 setGrade('5.13-')
                 break;
             }
         }
 
-    //parameters for checkboxes
-    const flashCheckbox = () => {
-        if( flashChecked === false) {
-            setFlashChecked(true);
-            setScore(score + 1);
+    const checkBonusPoints = (event, bonusPoints) => {
+        setBonusPoints(bonusPoints);
+        if('Flash'){
             setIsFlash(true);
-        } else {
-            setFlashChecked(false);
-            setScore(score-1);
-            setIsFlash(false);
-        }   
-        return score;
-    }
-
-    const onSightCheckbox = () => {
-        if( onSightChecked === false) {
-            setOnSightChecked(true);
             setScore(score + 1);
+        }
+        if('OnSight'){
             setIsOnSight(true);
-        } else {
-            setOnSightChecked(false);
-            setScore(score-1);
-            setIsOnSight(false);
-        }   
-        return score;
+            setScore(score + 1);
+        } 
+        if('Bonus'){
+            setIsBonus(true);
+            setScore(score + 1);
+        }  
     }
 
-    const bonusCheckbox = () => {
-        if( bonusChecked === false) {
-            setBonusChecked(true);
+    //parameters for choose climb dropdown
+    const chooseClimbType = (event, climbType) => {
+        setClimbType(climbType);
+        if ('Auto Belay') {
+            setScore(score);
+        } 
+        if ('Top Rope') {
             setScore(score + 1);
-            setIsBonus(true);
-        } else {
-            setBonusChecked(false);
-            setScore(score-1);
-            setIsBonus(false);
-        }   
+        } 
+        if ('Lead'){
+            setScore(score + 2);
+        }
         return score;
     }
 
     //submit climb function
     const submitClimb = () => {
+        compareGrade();
         let climb = {
             climbType,
             grade,
@@ -152,15 +122,15 @@ function AddClimb() {
         <div className="container">
             <form>
                 <div>
-                    <select 
+                    <ToggleButtonGroup
                         value={climbType}
-                        required
-                        onChange={chooseClimbType}>
-                            <option>CLIMB TYPE</option>
-                            <option value="Auto Belay">Auto Belay</option>
-                            <option value="Top Rope">Top Rope</option>
-                            <option value="Lead">Lead</option>
-                    </select>
+                        exclusive
+                        onChange={chooseClimbType}
+                    >
+                        <ToggleButton value="Auto Belay">AUTO BELAY</ToggleButton>
+                        <ToggleButton value="Top Rope">TOP ROPE</ToggleButton>
+                        <ToggleButton value="Lead">LEAD</ToggleButton>
+                    </ToggleButtonGroup>
                 </div>
                 <div>
                     <CircularInput
@@ -182,27 +152,22 @@ function AddClimb() {
                     </CircularInput>
                 </div>
                 <div>
-                    <h3>Flash:</h3>
-                    <Checkbox 
-                        checked={flashChecked}
-                        onChange={flashCheckbox}
-                        inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
-                    <h3>On Sight:</h3>
-                    <Checkbox 
-                        checked={onSightChecked}
-                        onChange={onSightCheckbox}
-                        inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
-                    <h3>Bonus:</h3>
-                    <Checkbox 
-                        checked={bonusChecked}
-                        onChange={bonusCheckbox}
-                        inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
+                    <ToggleButtonGroup
+                        value={bonusPoints}
+                        onChange={checkBonusPoints}
+                    >
+                        <ToggleButton value="Flash">FLASH</ToggleButton>
+                        <ToggleButton value="OnSight">ON SIGHT</ToggleButton>
+                        <ToggleButton value="Bonus">BONUS</ToggleButton>
+                    </ToggleButtonGroup>
                 </div>
                 <div>
                     <h2>SCORE: {score}</h2>
                 </div>
+                <div>
                 <Button variant="outlined" onClick={goBack}>BACK</Button>
                 <Button variant="outlined" onClick={submitClimb}>SUBMIT CLIMB</Button>
+                </div>
             </form>
         </div>
     )
